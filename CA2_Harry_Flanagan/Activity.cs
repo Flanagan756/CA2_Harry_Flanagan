594c6983e62241f531ebef93b39868389f46485c
Harry Flanagan <S00189591@mail.itsligo.ie>
2019-11-26 13:14:18 +0000

    Add project files.

---
+++ CA2_Harry_Flanagan/Activity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2_Harry_Flanagan
+{
+    /*enum*/
+    enum TypeOfActivity
+    {
+        Air,
+        Water,
+        Land
+    }
+    class Activity
+        {
+
+            /*Propterties*/
+            public string Name { get; set; }
+            public DateTime ActivityDate { get; set; }
+            public decimal Cost { get; set; }
+            public string Description { get; set; }
+            public TypeOfActivity TypeOfActivity { get; set; }
+
+        /*Constructors*/
+
+        public Activity(string name, string desctiption, DateTime activityDate, TypeOfActivity activityType, decimal cost)
+        {
+            Name = name;
+            Description = desctiption;
+            ActivityDate = activityDate;
+            TypeOfActivity = activityType;
+            Cost = cost;
+       
+            
+        }
+        //zero argument constructor
+        public Activity()
+        {
+
+        }
+
+        /*Methods*/
+        public override string ToString()
+        {
+            string shortDate = ActivityDate.ToShortDateString();
+            return $"{Name} - {shortDate}";
+        }
+    }
+ 
+}
---
+++ CA2_Harry_Flanagan/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CA2_Harry_Flanagan
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //create list
+        List<Activity> allActivities = new List<Activity>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Activity l1 = new Activity()
+            {
+                Name = "Treking",
+                Description = "Instructor led group trek through local mountains.",
+                ActivityDate = new DateTime(),
+                TypeOfActivity = TypeOfActivity.Land,
+                Cost = 20m
+            };
+
+            Activity l2 = new Activity()
+            {
+                Name = "Mountain Biking",
+                Description = "Instructor led half day mountain biking.  All equipment provided.",
+                ActivityDate = new DateTime(2019, 06, 02),
+                TypeOfActivity = TypeOfActivity.Land,
+                Cost = 30m
+            };
+
+            Activity l3 = new Activity()
+            {
+                Name = "Abseiling",
+                Description = "Experience the rush of adrenaline as you descend cliff faces from 10-500m.",
+                ActivityDate = new DateTime(2019, 06, 03),
+                TypeOfActivity = TypeOfActivity.Land,
+                Cost = 40m
+            };
+
+            Activity w1 = new Activity()
+            {
+                Name = "Kayaking",
+                Description = "Half day lakeland kayak with island picnic.",
+                ActivityDate = new DateTime(2019, 06, 01),
+                TypeOfActivity = TypeOfActivity.Water,
+                Cost = 40m
+            };
+
+            Activity w2 = new Activity()
+            {
+                Name = "Surfing",
+                Description = "2 hour surf lesson on the wild atlantic way",
+                ActivityDate = new DateTime(2019, 06, 02),
+                TypeOfActivity = TypeOfActivity.Water,
+                Cost = 25m
+            };
+
+            Activity w3 = new Activity()
+            {
+                Name = "Sailing",
+                Description = "Full day lakeland kayak with island picnic.",
+                ActivityDate = new DateTime(2019, 06, 03),
+                TypeOfActivity = TypeOfActivity.Water,
+                Cost = 50m
+            };
+
+            Activity a1 = new Activity()
+            {
+                Name = "Parachuting",
+                Description = "Experience the thrill of free fall while you tandem jump from an airplane.",
+                ActivityDate = new DateTime(2019, 06, 01),
+                TypeOfActivity = TypeOfActivity.Air,
+                Cost = 100m
+            };
+
+            Activity a2 = new Activity()
+            {
+                Name = "Hang Gliding",
+                Description = "Soar on hot air currents and enjoy spectacular views of the coastal region.",
+                ActivityDate = new DateTime(2019, 06, 02),
+                TypeOfActivity = TypeOfActivity.Air,
+                Cost = 80m
+            };
+
+            Activity a3 = new Activity()
+            {
+                Name = "Helicopter Tour",
+                Description = "Experience the ultimate in aerial sight-seeing as you tour the area in our modern helicopters",
+                ActivityDate = new DateTime(2019, 06, 03),
+                TypeOfActivity = TypeOfActivity.Air,
+                Cost = 200m
+            };
+
+            //add to list
+            allActivities.Add(l1);
+            allActivities.Add(l2);
+            allActivities.Add(l3);
+            allActivities.Add(w1);
+            allActivities.Add(w2);
+            allActivities.Add(w3);
+            allActivities.Add(a1);
+            allActivities.Add(a2);
+            allActivities.Add(a3);
+
+            //display within the listbox
+            lbxActivites.ItemsSource = allActivities;
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
